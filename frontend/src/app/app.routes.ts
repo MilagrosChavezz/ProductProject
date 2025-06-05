a69dc1261d947cbbc8c6d0faea4afedd9f70ecca
KevinLlombart <143675096+KevinLlombart@users.noreply.github.com>
2025-06-05 18:06:14 -0300

    Login view

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { OrderComponent } from './order/order.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'order', component: OrderComponent },
   { path: 'products', loadChildren: () => import('./products/products.routes').then(m => m.routes) },
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: 'home' } 
 ];
